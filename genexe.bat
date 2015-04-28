6d02799ad13b029627eedd21b45cc2f1eec40467
honorabrutroll <edward.yang6771@gmail.com>
2015-04-28 12:32:30 -0500

    fixed batch file

--- genexe.bat
+++ genexe.bat
@@ -10,7 +10,7 @@ IF EXIST "%~dp0dist\windows" rmdir /s /q "%~dp0dist\windows"
 @REM If the file wasn't deleted for some reason, stop and error
 IF EXIST "%~dp0dist\windows" exit 1
 ECHO "Running Tests..."
-"%racketdir%\Racket.exe" "%~dp0src\test.rkt"
+"%racketdir%\Racket.exe" "%~dp0src\tests.rkt"
 IF NOT ERRORLEVEL 0 GOTO testfail
 ECHO "Compiling..."
 "%racketdir%\raco.exe" exe "%~dp0src\main.rkt"
